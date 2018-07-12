48baca2485a1867a9b8f3140d3fa5771953ad045
Arthur McGregor <arthur.mcgregor@gmail.com>
2018-07-12 13:10:47 +1200

    Populate recordings table (WIP) Search function not refining results correctly No view link yet

---
+++ src/components/TableRecordings.vue
@@ -0,0 +1,67 @@
+<template>
+  <b-table
+  striped
+  hover
+  responsive
+  v-bind:items="items"
+  v-bind:fields="fields"></b-table>
+</template>
+
+<script>
+
+export default {
+  // https://vuejs.org/v2/style-guide/#Multi-word-component-names-essential
+  name: 'table-recordings',
+  // https://vuejs.org/v2/style-guide/#Component-data-essential
+  data () {
+    return {
+      fields: [
+        {
+          key: 'view_link',
+          label: 'View'
+        },
+        {
+          key: 'id'
+        },
+        {
+          key: 'devicename',
+          label: 'Device'
+        },
+        {
+          key: 'groupname',
+          label: 'Group'
+        },
+        {
+          key: 'location'
+        },
+        {
+          key: 'time'
+        },
+        {
+          key: 'date'
+        },
+        {
+          key: 'duration'
+        },
+        {
+          key: 'tags'
+        },
+        {
+          key: 'other'
+        },
+        {
+          key: 'processing_state'
+        },
+      ]
+    }
+  },
+
+// https://vuejs.org/v2/style-guide/#Simple-computed-properties-strongly-recommended
+computed: {
+},
+// https://vuejs.org/v2/style-guide/#Prop-definitions-essential
+props: {
+  items: Array
+},
+}
+</script>
--- src/views/RecordingsView.vue
+++ src/views/RecordingsView.vue
@@ -4,9 +4,7 @@
 		heading="Search Video Recordings"
 		v-model="query"
 		v-on:getRecordings="getRecordings"/>
-		<div>
-			{{ recordings }}
-		</div>
+		<TableRecordings v-bind:items="tableItems"/>
 
 	</b-container>
 </template>
@@ -24,7 +22,8 @@ export default {
 	data () {
 		return {
 			query: {},
-			recordings: []
+			recordings: [],
+			tableItems: []
 		}
 	},
 	// https://vuejs.org/v2/style-guide/#Simple-computed-properties-strongly-recommended
@@ -49,9 +48,57 @@ export default {
 						reject(json)
 					} else {
 						this.recordings = json.rows
+						json.rows.map((row) => {
+							this.tableItems.push({
+								view_link: this.parseViewLink(row.id),
+								id: row.id,
+								devicename: row.Device.devicename,
+								groupname: row.Group.groupname,
+								location: this.parseLocation(row.location),
+								date: new Date(row.recordingDateTime).toLocaleDateString('en-NZ'),
+								time: new Date(row.recordingDateTime).toLocaleTimeString(),
+								duration: row.duration,
+								tags: this.parseTags(row.Tags),
+								other: '',
+								processing_state: this.parseProcessingState(row.processingState)
+							})
+						})
+						console.log(json.rows) //eslint-disable-line
 					}
 				})
 			})
+		},
+		parseLocation(location) {
+			if (location && typeof location === 'object') {
+				let latitude = location.coordinates[0];
+				let longitude = location.coordinates[1];
+				return latitude + ', ' + longitude
+			} else {
+				return "(unknown)"
+			}
+		},
+		parseTags(tags) {
+			let animal = (tag) => {
+				if (!tag.animal) {
+					return "F/P"
+				} else {
+					return tag.animal
+				}
+			}
+			let tagString = animal(tags[0])
+			for (let i = 1; i < tags.length; i ++) {
+				let animalName = animal(tags[i])
+				tagString = tagString + ', ' + animalName
+			}
+			return tagString
+		},
+		parseProcessingState(result) {
+			let string = result.toLowerCase();
+			return string.charAt(0).toUpperCase() + string.slice(1);
+		},
+		parseViewLink(id) {
+			id
+			return "(view link here)"
 		}
 	}
 }
