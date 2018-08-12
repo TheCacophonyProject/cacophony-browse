0156d49fabfa734cae27472cef5e129cbc734773
adbs1 <adbs1@yahoo.com>
2018-08-13 05:14:47 +1200

    Configuration for production build. See the README for details.

--- app.config.js
+++ app.config.js
@@ -2,16 +2,16 @@ let api;
 
 switch(process.env.NODE_ENV) {
   case 'production':
-    api = "http://localhost:1080";
+    api = "https://localhost:1080";
     break;
   case 'development':
-    api = "http://localhost:1080";
+    api = "https://localhost:1080";
     break;
   case 'test':
     api = "http://mocked-api-path";
     break;
   default:
-    api = "http://localhost:1080";
+    api = "https://localhost:1080";
 }
 
 module.exports = {
