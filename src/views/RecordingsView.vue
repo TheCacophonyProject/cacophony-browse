a1659b8c5cc30942b8e6a0ca452419720615960d
Menno Finlay-Smits <inbox@menno.io>
2018-09-20 21:52:41 +1200

    Default to 300 recordings per page

--- src/views/RecordingsView.vue
+++ src/views/RecordingsView.vue
@@ -75,12 +75,13 @@ export default {
       count: null,
       countMessage: null,
       currentPage: null,
-      perPage: 100,
+      perPage: 300,
       limitPaginationButtons: 5,
       perPageOptions: [
         {value: 10, text: "10 per page"},
         {value: 50, text: "50 per page"},
         {value: 100, text: "100 per page"},
+        {value: 300, text: "300 per page"},
         {value: 500, text: "500 per page"},
         {value: 1000, text: "1000 per page"}
       ]
