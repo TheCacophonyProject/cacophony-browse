8c1dc3287f109ac0a80a260098e8071b52c5a82c
Jimmy Kirkus-Lamont <jimmy.kl@gmail.com>
2018-10-01 22:44:42 +1300

    Prev/Next button icons
    Change buttons to fontawesome icons
    Update help
    Remove old images
    
    Fixes #66

--- src/components/Video/PrevNext.vue
+++ src/components/Video/PrevNext.vue
@@ -1,35 +1,75 @@
 <template>
   <div class="img-buttons">
-    <img
-      class="img-button"
-      title="Previous for device, skipping bird &amp; false-positives"
-      src="../../assets/video/previous-interesting.png"
+    <span 
+      title="Previous for device, skipping bird &amp; false-positives" 
       @click="nextRecording('previous', 'any', ['interesting'])">
-    <img
-      class="img-button"
-      title="Previous for device, not manually tagged"
-      src="../../assets/video/previous-no-human.png"
+      <font-awesome-layers class="fa-fw">
+        <font-awesome-icon 
+          icon="asterisk"
+          style="color: red"
+          transform="up-24 right-10 shrink-3"
+          value="?" />
+        <font-awesome-icon 
+          class="fa-3x"
+          icon="angle-double-left" />
+      </font-awesome-layers>
+    </span>
+    <span 
+      title="Previous for device, not manually tagged" 
       @click="nextRecording('previous', 'no-human')">
-    <img
-      class="img-button"
-      title="Previous for device"
-      src="../../assets/video/previous.png"
+      <font-awesome-layers class="fa-fw">
+        <font-awesome-icon 
+          icon="question"
+          style="color: green"
+          transform="up-24 right-10 shrink-3"
+          value="?" />
+        <font-awesome-icon 
+          class="fa-3x"
+          icon="angle-left" />
+      </font-awesome-layers>
+    </span>
+    <span 
+      title="Previous for device" 
       @click="nextRecording('previous', 'any')">
-    <img
-      class="img-button"
-      title="Next for device"
-      src="../../assets/video/next.png"
-      @click="nextRecording('next', 'any')" >
-    <img
-      class="img-button"
-      title="Next for device, not manually tagged"
-      src="../../assets/video/next-no-human.png"
-      @click="nextRecording('next', 'no-human')" >
-    <img
-      class="img-button"
-      title="Next for device, skipping birds &amp; false-positives"
-      src="../../assets/video/next-interesting.png"
-      @click="nextRecording('next', 'any', ['interesting'])" >
+      <font-awesome-icon 
+        icon="angle-left" 
+        size="3x" />
+    </span>
+    <span 
+      title="Next for device" 
+      @click="nextRecording('next', 'any')">
+      <font-awesome-icon 
+        icon="angle-right" 
+        size="3x" />
+    </span>
+    <span 
+      title="Next for device, not manually tagged" 
+      @click="nextRecording('next', 'no-human')">
+      <font-awesome-layers class="fa-fw">
+        <font-awesome-icon 
+          class="fa-3x"
+          icon="angle-right" />
+        <font-awesome-icon 
+          icon="question"
+          style="color: green"
+          transform="up-24 left-10 shrink-3"
+          value="?" />
+      </font-awesome-layers>
+    </span>
+    <span 
+      title="Next for device, skipping birds &amp; false-positives" 
+      @click="nextRecording('next', 'any', ['interesting'])">
+      <font-awesome-layers class="fa-fw">
+        <font-awesome-icon 
+          class="fa-3x"
+          icon="angle-double-right" />
+        <font-awesome-icon 
+          icon="asterisk"
+          style="color: red"
+          transform="up-24 left-10 shrink-3"
+          value="?" />
+      </font-awesome-layers>
+    </span>
   </div>
 </template>
 
@@ -64,10 +104,16 @@ export default {
   padding: 1em;
   text-align: center;
 }
-.img-button {
-  cursor: pointer;
-  box-sizing: border-box;
-  width: 4em;
-  max-height: 4em;
-}
+.img-buttons
+  span {
+    cursor: pointer;
+    width: 4em;
+    max-height: 4em;
+    box-sizing: border-box;
+    display: inline-block;
+    opacity: 0.6;
+  }
+  span:hover {
+    opacity: 1;
+  }
 </style>
