a712f35f0b860dd07d1d62a230991dda8ec1a35b
Menno Finlay-Smits <inbox@menno.io>
2018-10-05 18:08:56 +1300

    Fix up prev/next buttons
    Since the FontAwesomeLayers import fix the layout of the prev/next
    buttons has been messed up. It turns out the correct layout is
    achieved by not using `font-awesome-layers` at all!

--- src/components/Video/VideoHelp.vue
+++ src/components/Video/VideoHelp.vue
@@ -16,55 +16,47 @@
         <p class="col-xs-6">Move between recordings for device</p>
       </b-col>
       <b-col cols="5">
-        <font-awesome-layers class="fa-fw">
-          <font-awesome-icon 
-            icon="question"
-            style="color: green"
-            transform="up-16 shrink-7"
-            value="?" />
-          <font-awesome-icon 
-            class="fa-2x"
-            icon="angle-left" 
-            transform="left-5" />
-        </font-awesome-layers>
-        <font-awesome-layers class="fa-fw">
-          <font-awesome-icon 
-            class="fa-2x"
-            icon="angle-right" 
-            transform="right-5" />
-          <font-awesome-icon 
-            icon="question"
-            style="color: green"
-            transform="up-16 shrink-7"
-            value="?" />
-        </font-awesome-layers>
+        <font-awesome-icon
+          icon="question"
+          style="color: green"
+          transform="up-16 shrink-7"
+          value="?" />
+        <font-awesome-icon
+          class="fa-2x"
+          icon="angle-left"
+          transform="left-5" />
+        <font-awesome-icon
+          class="fa-2x"
+          icon="angle-right"
+          transform="right-5" />
+        <font-awesome-icon
+          icon="question"
+          style="color: green"
+          transform="up-16 shrink-7"
+          value="?" />
       </b-col>
       <b-col cols="7">
         <p class="col-xs-6">Move between recordings for device which have not been manually tagged</p>
       </b-col>
       <b-col cols="5">
-        <font-awesome-layers class="fa-fw">
-          <font-awesome-icon 
-            icon="asterisk"
-            style="color: red"
-            transform="up-16 shrink-7"
-            value="?" />
-          <font-awesome-icon 
-            class="fa-2x"
-            icon="angle-double-left" 
-            transform="left-5" />
-        </font-awesome-layers>
-        <font-awesome-layers class="fa-fw">
-          <font-awesome-icon 
-            class="fa-2x"
-            icon="angle-double-right" 
-            transform="right-5" />
-          <font-awesome-icon 
-            icon="asterisk"
-            style="color: red"
-            transform="up-16 shrink-7"
-            value="?" />
-        </font-awesome-layers>
+        <font-awesome-icon
+          icon="asterisk"
+          style="color: red"
+          transform="up-16 shrink-7"
+          value="?" />
+        <font-awesome-icon
+          class="fa-2x"
+          icon="angle-double-left"
+          transform="left-5" />
+        <font-awesome-icon
+          class="fa-2x"
+          icon="angle-double-right"
+          transform="right-5" />
+        <font-awesome-icon
+          icon="asterisk"
+          style="color: red"
+          transform="up-16 shrink-7"
+          value="?" />
       </b-col>
       <b-col cols="7">
         <p class="col-xs-6">Move between 'interesting' tagged recordings for device where the first tag is not a a bird or false/positive</p>
