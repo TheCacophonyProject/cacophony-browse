57848bf465e135c2bba41a3c2d975b8799e88fea
Arthur McGregor <arthur.mcgregor@gmail.com>
2019-03-19 21:01:19 +1300

    UI/UX Improvements to homepage

---
+++ src/components/HomeGroups.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h3>Your groups</h3>
+    <p>Click on a group to view the last 24 hours of recordings in that group</p>
+
+    <b-list-group>
+      <HomeGroupItem
+        v-for="group in groups"
+        :key="group.id"
+        :group="group"
+      />
+    </b-list-group>
+  </div>
+</template>
+
+<script>
+
+import HomeGroupItem from './HomeGroupItem.vue';
+import { mapState } from 'vuex';
+
+export default {
+  name: "HomeGroups",
+  components: { HomeGroupItem },
+  computed: mapState({
+    groups: state => state.Groups.groups,
+  }),
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/HomeUser.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h3>Your details</h3>
+
+    <dl>
+      <dt>Username</dt>
+      <dd id="username">{{ username }}</dd>
+      <dt v-show="firstname">First name</dt>
+      <dd
+        v-show="firstname"
+        id="firstname">{{ firstname }}
+      </dd>
+      <dt v-show="lastname">Last name
+      </dt>
+      <dd
+        v-show="lastname"
+        id="lastname">{{ lastname }}
+      </dd>
+      <dt>Email</dt>
+      <dd id="email">{{ email }}</dd>
+
+    </dl>
+  </div>
+</template>
+
+<script>
+
+import {mapState} from 'vuex';
+
+export default {
+  name: "HomeUser",
+  computed: mapState({
+    user: state => state.User,
+    username: state => state.User.userData.username,
+    firstname: state => state.User.userData.firstname,
+    lastname: state => state.User.userData.lastname,
+    email: state => state.User.userData.email,
+  }),
+};
+</script>
+
+<style scoped>
+
+</style>
