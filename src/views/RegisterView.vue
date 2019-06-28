b4ba3e46f959866ed4f93daa30eb5626827273b5
Sara Coutinho <sara@saracoutinho.co.nz>
2019-06-28 16:28:01 +1200

    UI changes to login and register screens Changed behaviour of login button to match register (disabled until fields are filled)

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,79 +1,91 @@
 <template>
-  <b-container class="col-md-6 col-lg-5">
-    <h1>Register</h1>
+  <b-container class="sign-wrapper">
+    <b-form-row class="align-items-center justify-content-center">
+      <div class="sign-form-wrapper bg-white rounded">
 
-    <b-form @submit="onSubmit">
+        <div class="logo" />
 
-      <b-alert
-        :show="!!errorMessage"
-        variant="danger"
-        dismissible
-        @dismissed="errorMessage=undefined">
-        {{ errorMessage }}
-      </b-alert>
+        <h1 class="text-center">Register</h1>
 
-      <b-form-group
-        :state="!$v.form.username.$error"
-        :invalid-feedback="usernameFeedback"
-        label="Username"
-        label-for="input-username"
-      >
-        <b-form-input
-          id="input-username"
-          v-model="$v.form.username.$model"
-          :state="!$v.form.username.$error"
-          type="text"
-          autofocus
-          required
-        />
-      </b-form-group>
+        <b-form @submit="onSubmit">
 
-      <b-form-group
-        :state="!$v.form.email.$error"
-        label="Email"
-        label-for="input-email"
-      >
-        <b-form-input
-          id="input-email"
-          v-model="$v.form.email.$model"
-          :state="!$v.form.email.$error"
-          type="email"
-          required
-        />
-      </b-form-group>
+          <b-alert
+            :show="!!errorMessage"
+            variant="danger"
+            dismissible
+            @dismissed="errorMessage=undefined">
+            {{ errorMessage }}
+          </b-alert>
 
-      <b-form-group
-        :state="!$v.form.password.$error"
-        :invalid-feedback="passwordFeedback"
-        label="Password"
-        label-for="input-password"
-      >
-        <b-form-input
-          id="input-password"
-          v-model="$v.form.password.$model"
-          :state="!$v.form.password.$error"
-          type="password"/>
-      </b-form-group>
+          <b-form-group
+            :state="!$v.form.username.$error"
+            :invalid-feedback="usernameFeedback"
+            label="Username"
+            label-for="input-username"
+          >
+            <b-form-input
+              id="input-username"
+              v-model="$v.form.username.$model"
+              :state="!$v.form.username.$error"
+              type="text"
+              autofocus
+              required
+            />
+          </b-form-group>
 
-      <b-form-group
-        :state="!$v.form.passwordConfirm.$error"
-        :invalid-feedback="passwordConfirmFeedback"
-        label="Retype password"
-        label-for="input-password-retype"
-      >
-        <b-form-input
-          v-model="$v.form.passwordConfirm.$model"
-          :state="!$v.form.passwordConfirm.$error"
-          type="password"/>
-      </b-form-group>
+          <b-form-group
+            :state="!$v.form.email.$error"
+            label="Email"
+            label-for="input-email"
+          >
+            <b-form-input
+              id="input-email"
+              v-model="$v.form.email.$model"
+              :state="!$v.form.email.$error"
+              type="email"
+              required
+            />
+          </b-form-group>
 
-      <b-button
-        :disabled="$v.form.$invalid"
-        type="submit"
-        variant="primary">Register
-      </b-button>
+          <b-form-group
+            :state="!$v.form.password.$error"
+            :invalid-feedback="passwordFeedback"
+            label="Password"
+            label-for="input-password"
+          >
+            <b-form-input
+              id="input-password"
+              v-model="$v.form.password.$model"
+              :state="!$v.form.password.$error"
+              type="password"/>
+          </b-form-group>
 
-    </b-form>
+          <b-form-group
+            :state="!$v.form.passwordConfirm.$error"
+            :invalid-feedback="passwordConfirmFeedback"
+            label="Retype password"
+            label-for="input-password-retype"
+          >
+            <b-form-input
+              v-model="$v.form.passwordConfirm.$model"
+              :state="!$v.form.passwordConfirm.$error"
+              type="password"/>
+          </b-form-group>
+
+          <b-button
+            :disabled="$v.form.$invalid"
+            type="submit"
+            class="btn-block"
+            variant="primary">Register
+          </b-button>
+
+          <p class="small mt-4 text-center">Already registered? <b-link href="/login">Login here</b-link>.</p>
+
+        </b-form>
+
+      </div>
+    </b-form-row>
+
   </b-container>
 </template>
 
@@ -161,3 +173,11 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+  .sign-form-wrapper {
+    max-width: 460px;
+  }
+</style>
+
+
