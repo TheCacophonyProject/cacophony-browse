7c218b7032a59fce34bbdba90289371b18136ba1
GP <g.ferraro22@gmail.com>
2020-01-09 15:51:11 +1300

    fixed bug with page not loading for some urls (#276)
    * fixed bug with page not loading for some urls

--- src/components/QueryRecordings/QueryRecordings.vue
+++ src/components/QueryRecordings/QueryRecordings.vue
@@ -266,7 +266,8 @@ export default {
   mounted() {
     this.isAudio = this.recordingType === "audio";
     this.parseCurrentRoute();
-    this.updateRouteQuery();
+    this.saveLastQuery();
+    this.$emit("submit", this.serialiseQuery(this.query, true));
   },
   updated() {
     if (!this.loadedQuery) {
@@ -283,6 +284,9 @@ export default {
     }
   },
   methods: {
+    saveLastQuery() {
+      this.lastQuery = JSON.parse(JSON.stringify(this.query));
+    },
     updatePagination(perPage, page) {
       this.query.limit = perPage;
       const newOffset = Math.max(0, (page - 1) * perPage);
@@ -341,7 +345,7 @@ export default {
     },
     updateRouteQuery() {
       // Update the url query params string so that this search can be easily shared.
-      this.lastQuery = JSON.parse(JSON.stringify(this.query));
+      this.saveLastQuery();
       this.$router.push({
         path: this.path,
         query: this.serialiseQuery(this.query)
