83484f54158937aef6693a3d708c823be2c0acdd
Jon Hardie <jon@hardiesoft.com>
2021-02-03 16:32:08 +1300

    Load api key from external source (2)

--- webpack/webpack.release.js
+++ webpack/webpack.release.js
@@ -24,8 +24,7 @@ module.exports = merge(common, {
     new webpack.DefinePlugin({
       __ENV__: JSON.stringify("PRODUCTION"),
       __API__: JSON.stringify("https://api.cacophony.org.nz"),
-      // TODO(jon): Needs to come from a file outside of VCS
-      __LINZ_API_KEY__: JSON.stringify("YOUR_API_KEY_HERE"),
+      __LINZ_API_KEY__: JSON.stringify(process.env.LINZ_BASEMAP_API_KEY || ""),
       __ENV_STAGING__: JSON.stringify("STAGING"),
       __API_STAGING__: JSON.stringify("https://api-test.cacophony.org.nz"),
     }),
