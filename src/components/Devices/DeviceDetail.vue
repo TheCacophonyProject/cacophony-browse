3d41f1a80e17f6ab260e7f21d0b54691755f5186
Clare McLennan <>
2021-03-09 11:04:26 +1300

    Device page now has tabs

--- src/components/Devices/DeviceDetail.vue
+++ src/components/Devices/DeviceDetail.vue
@@ -1,23 +1,40 @@
 <template>
-  <div class="container">
-    <b-row>
-      <b-col class="col-12 col-lg-7">
-        <device-users :device="device" :user="user" />
-        <hr />
-        <device-software :software="software" />
-      </b-col>
-    </b-row>
-  </div>
+  <b-container>
+    <b-tabs
+      card
+      class="device-tabs"
+      nav-class="device-tabs-container"
+      v-model="currentTabIndex"
+    >
+      <b-tab title="Current Software" >
+        <DeviceSoftware :software="software"/>
+      </b-tab>
+      <b-tab title="Users">
+        <template #title>
+            <TabTemplate
+              title= "Users"
+              :isLoading="!device"
+              :value="(device && device.Users) ? device.Users.length : 0" />
+        </template>
+        <DeviceUsers
+          :device="device"
+          :user="user"
+        />
+      </b-tab>
+    </b-tabs>
+  </b-container>
 </template>
 
 <script>
 import { mapState } from "vuex";
 import DeviceUsers from "./DeviceUsers.vue";
 import DeviceSoftware from "./DeviceSoftware.vue";
+import TabTemplate from '@/components/TabTemplate.vue';
+import Help from "../../components/Help.vue"
 
 export default {
   name: "DeviceDetail",
-  components: { DeviceUsers, DeviceSoftware },
+  components: { DeviceUsers, DeviceSoftware, TabTemplate, Help },
   props: {
     device: {
       type: Object,
@@ -32,11 +49,22 @@ export default {
       required: true,
     },
   },
+  data()  { 
+    return {
+      currentTabIndex: 0
+    };
+  },
 };
 </script>
-<style scoped>
-hr {
-  margin-top: 2em;
-  width: 60%;
+<style lang="scss">
+.device-tabs {
+  .card-header {
+    // Same color as the jumbotron component abutting above the tabs.
+    background-color: #f8f9fa;
+    .card-header-tabs {
+      margin-left: auto;
+      margin-right: auto;
+    }
+  }
 }
 </style>
---
+++ src/components/TabTemplate.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <span>{{ title }}</span>
+        <b-spinner v-if="isLoading" type="border" small />
+        <b-badge v-else-if="value" pill variant="secondary">{{
+        value
+        }}</b-badge>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "TabTemplate",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    }, 
+    isLoading: {
+        type: Boolean, 
+        default: false,
+    },
+    value: {
+        type: Number,
+        default: 0,
+    }
+  },
+}
+</script>
+
+<style>
+</style>
