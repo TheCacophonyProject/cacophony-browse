5d9c92004f90fb1839c7927abcf697c489d06722
Jon Hardie <jon@hardiesoft.com>
2021-03-08 16:13:21 +1300

    Fix router

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 // Leaflet CSS
 import "leaflet/dist/leaflet.css";
+import Router from "vue-router";
 
 export default function () {
   // https://bootstrap-vue.js.org/docs
@@ -21,6 +22,8 @@ export default function () {
   //https://monterail.github.io/vuelidate
   Vue.use(Vuelidate);
 
+  Vue.use(Router);
+
   Vue.component("font-awesome-icon", FontAwesomeIcon);
 
   // https://vue-multiselect.js.org
@@ -28,6 +31,7 @@ export default function () {
 
   new Vue({
     el: "#app",
+    // @ts-ignore
     store,
     router,
     render: (h) => h(App),
